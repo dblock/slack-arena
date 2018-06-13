41c441b6c180e1a5572062f5b80fb7c7e2e0d940
dblock <dblock@dblock.org>
2018-06-13 07:35:17 -0400

    Attempt to restart down teams.

--- slack-arena/app.rb
+++ slack-arena/app.rb
@@ -12,6 +12,9 @@ module SlackArena
       once_and_every 2 * 60 do
         sync!
       end
+      once_and_every 60 * 60 do
+        ping_teams!
+      end
     end
 
     private
@@ -26,6 +29,25 @@ module SlackArena
       yield
       every tt do
         yield
+      end
+    end
+
+    def ping_teams!
+      Team.active.each do |team|
+        begin
+          ping = team.ping!
+          next if ping[:presence].online
+          logger.warn "DOWN: #{team}"
+          after 60 do
+            ping = team.ping!
+            unless ping[:presence].online
+              logger.info "RESTART: #{team}"
+              SlackStrava::Service.instance.start!(team)
+            end
+          end
+        rescue StandardError => e
+          logger.warn "Error pinging team #{team}, #{e.message}."
+        end
       end
     end
 
