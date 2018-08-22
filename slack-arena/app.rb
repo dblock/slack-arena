00434b944d771964a460d7d30172edf5fc61db4f
dblock <dblock@dblock.org>
2018-08-22 09:07:16 -0400

    Upgraded dependencies, slack-ruby-bot-server 0.7.0.

--- slack-arena/app.rb
+++ slack-arena/app.rb
@@ -12,9 +12,6 @@ module SlackArena
       once_and_every 2 * 60 do
         sync!
       end
-      once_and_every 60 * 3 do
-        ping_teams!
-      end
     end
 
     private
@@ -29,23 +26,6 @@ module SlackArena
       yield
       every tt do
         yield
-      end
-    end
-
-    def ping_teams!
-      Team.active.each do |team|
-        ping = team.ping!
-        next if ping[:presence].online
-        logger.warn "DOWN: #{team}"
-        after 60 do
-          ping = team.ping!
-          unless ping[:presence].online
-            logger.info "RESTART: #{team}"
-            SlackArena::Service.instance.start!(team)
-          end
-        end
-      rescue StandardError => e
-        logger.warn "Error pinging team #{team}, #{e.message}."
       end
     end
 
