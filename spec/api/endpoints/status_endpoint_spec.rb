895f55aca8f069e6db0796ce3ab38b6f7dc0e29e
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2024-03-20 16:16:26 -0400

    Present an active team.

--- spec/api/endpoints/status_endpoint_spec.rb
+++ spec/api/endpoints/status_endpoint_spec.rb
@@ -14,13 +14,22 @@ describe Api::Endpoints::StatusEndpoint do
       expect(status._resource._links['self'].to_s).to eq 'http://example.org/api/status'
     end
 
-    context 'with a team' do
-      let!(:team) { Fabricate(:team, active: false) }
+    context 'with an active team' do
+      let!(:team) { Fabricate(:team) }
       it 'returns a status with ping' do
         status = client.status
         expect(status.teams_count).to eq 1
         ping = status.ping
         expect(ping['ok']).to eq 1
+      end
+    end
+
+    context 'with an inactive team' do
+      let!(:team) { Fabricate(:team, active: false) }
+      it 'returns a status without ping' do
+        status = client.status
+        expect(status.teams_count).to eq 1
+        expect(status).to_not respond_to(:ping)
       end
     end
   end
